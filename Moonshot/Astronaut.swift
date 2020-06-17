c2e2251002e5656f19f7bec3b985a2aa0c6184fb
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-06-17 16:27:59 +0900

    add Project 8, part 2

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by KazukiNakano on 2020/06/17.
+//  Copyright © 2020 kazu. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
